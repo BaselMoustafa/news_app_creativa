349f668dce61bd6a809f24f317a6cfd8d64e0b48
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-08-25 21:02:17 +0300

    finalize filter news screen

--- lib/screens/news_filter_screen.dart
+++ lib/screens/news_filter_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/models/country.dart';
+import 'package:news_app/styles/app_text_styles.dart';
+import 'package:news_app/widgets/apply_filter_button.dart';
+import 'package:news_app/widgets/category_selector_wigdet.dart';
 import 'package:news_app/widgets/country_selector_widget.dart';
-import 'package:news_app/widgets/selection_card_widget.dart';
 
 class NewsFilterScreen extends StatelessWidget {
   const NewsFilterScreen({super.key});
@@ -12,15 +13,43 @@ class NewsFilterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('News Filter'),
       ),
-      body: Column(
-        children: [
-          CountrySelectorWidget(
-            initialCountry: Country.countries[2],
-            onChnaged: (country){
-              print("Country changed to ${country.name}");
-            },
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          spacing: 10,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Select Country",
+              style: AppTextStyles.bold18,
+            ),
+            CountrySelectorWidget(
+              initialCountry:null,
+              onChnaged: (country){
+                print("Country changed to $country");
+              },
+            ),
+        
+            SizedBox(height: 10,),
+        
+            Text(
+              "Select Category",
+              style: AppTextStyles.bold18,
+            ),
+            CategorySelectorWidget(
+              initialCategory:null,
+              onChnaged: (category){
+                print("Category changed to $category");
+              },
+            ),
+
+            Spacer(),
+
+            ApplyFilterButton(),
+
+            SizedBox(height: 20,)
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/apply_filter_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/styles/app_colors.dart';
+import 'package:news_app/styles/app_text_styles.dart';
+
+class ApplyFilterButton extends StatelessWidget {
+  const ApplyFilterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        height: 48,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.primary
+        ),
+        child: Center(
+          child: Text(
+            "Apply Filters",
+            style: AppTextStyles.regular16.copyWith(
+              color: AppColors.white
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTap() {
+      
+    }
+}
