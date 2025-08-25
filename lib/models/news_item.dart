2b1df8de8f6b0e8c34d3132428aa25cc97a3d574
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-08-25 18:37:16 +0300

    model created

---
+++ lib/models/news_item.dart
@@ -0,0 +1,17 @@
+class NewsItem {
+  final String title;
+  final String describtion;
+  final String url;
+  final String imageUrl;
+  final String author;
+  final DateTime date;
+
+  const NewsItem({
+    required this.title,
+    required this.describtion,
+    required this.url,
+    required this.imageUrl,
+    required this.author,
+    required this.date
+  });
+}
